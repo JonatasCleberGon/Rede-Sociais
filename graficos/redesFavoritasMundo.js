99a82306ad90b939d456418329f8ffad2055919c
JonatasCleberGon <166825639+JonatasCleberGon@users.noreply.github.com>
2024-11-08 08:57:09 -0300

    .

--- graficos/redesFavoritasMundo.js
+++ graficos/redesFavoritasMundo.js
@@ -1,5 +1,8 @@
 async function redesFavoritasMundo() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
     const res = await fetch(url)
-    const dados =
+    const dados = await res.json()
+    console.log(dados);
 }
+
+redesFavoritasMundo()
